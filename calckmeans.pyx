8cd110516dcd541f86d058ad82568f115ed779ce
masyud303 <yudhi.prabowo86@gmail.com>
2019-11-06 17:07:02 +0700

    Add files via upload

---
+++ calckmeans.pyx
@@ -0,0 +1,61 @@
+#cython: language_level=3
+
+cimport cython
+cimport numpy as np
+import numpy as np
+from libc.math cimport sqrt
+from libc.stdio cimport printf
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cpdef unsigned char[:,::1] kmeans_calc(unsigned char[:,:,::1] img, int row, int col, int k):
+    cdef:
+        int i, j, d, idk, a
+        double dmin, distm, dist, temp
+        unsigned char[::1] dn
+        unsigned char[:,::1] kmeans
+        double[:,::1] mean, accum
+    
+    kmeans = np.zeros((row, col), dtype=np.uint8)
+    mean = np.zeros((k, 3), dtype=np.double)
+    accum = np.zeros((k, 4), dtype=np.double)
+    dn = np.zeros(k, dtype=np.uint8)
+    
+    distm = 0
+    for i in range(k):
+        mean[i, 0] = int(float(i+1)/(k+1)*256)
+        mean[i, 1] = int(float(i+1)/(k+1)*256)
+        mean[i, 2] = int(float(i+1)/(k+1)*256)
+        distm += mean[i, 0]**2 + mean[i, 1]**2 + mean[i, 2]**2
+        dn[i] = int(i/float(k-1)*255)
+    distm = distm//(3*k)
+    temp = 0
+    while(distm != temp):
+        temp = distm
+        accum[:, :] = 0
+        for i in range(row):
+            for j in range(col):
+                for d in range(k):
+                    dist = sqrt((mean[d, 0] - img[0, i, j])**2 + (mean[d, 1] - img[1, i, j])**2 + (mean[d, 2] - img[2, i, j])**2)
+                    if(d == 0):
+                        dmin = dist
+                        idk = d
+                    if(d > 0 and dist < dmin):
+                        dmin = dist
+                        idk = d
+                kmeans[i, j] = dn[idk]
+                accum[idk, 0] += img[0, i, j]
+                accum[idk, 1] += img[1, i, j]
+                accum[idk, 2] += img[2, i, j]
+                accum[idk, 3] += 1
+        distm = 0
+        for i in range(k):
+            mean[i, 0] = int(accum[i, 0]//accum[i, 3])
+            mean[i, 1] = int(accum[i, 1]//accum[i, 3])
+            mean[i, 2] = int(accum[i, 2]//accum[i, 3])
+            distm += mean[i, 0]**2 + mean[i, 1]**2 + mean[i, 2]**2
+        distm = distm//(3*k)
+        printf("%lf %lf\n", distm, temp)
+    
+    return kmeans
---
+++ kmeans.py
@@ -0,0 +1,44 @@
+import gdal
+import numpy as np
+import calckmeans as ck
+
+inp = "F:\\kegiatan\\2019\\INSINAS\\DATA\\1_res5.tif"
+out = "F:\\kegiatan\\2019\\INSINAS\\DATA\\1_res5_kmeans4.tif"
+
+###############################################################################
+
+def writearr(out, img, dtype, col, row, band, gt, proj, nodata):
+    ## data type code : "uint8 and int8": 1, "uint16": 2, "int16": 3,
+    ## "uint32": 4, "int32": 5, "float32": 6, "float64": 7
+    if(dtype == 1):
+        driver = gdal.GetDriverByName('GTIFF').Create(out, col, row, band, gdal.GDT_Byte)
+    if(dtype == 2):
+        driver = gdal.GetDriverByName('GTIFF').Create(out, col, row, band, gdal.GDT_UInt16)
+    if(dtype == 4):
+        driver = gdal.GetDriverByName('GTIFF').Create(out, col, row, band, gdal.GDT_UInt32)
+    if(dtype == 6):
+        driver = gdal.GetDriverByName('GTIFF').Create(out, col, row, band, gdal.GDT_Float32)
+    if(dtype == 7):
+        driver = gdal.GetDriverByName('GTIFF').Create(out, col, row, band, gdal.GDT_Float64)
+    driver.SetGeoTransform(gt)
+    driver.SetProjection(proj)
+    for i in range(band):
+        if(nodata != -1):
+            driver.GetRasterBand(i+1).SetNoDataValue(nodata)
+        if(band > 1):
+            driver.GetRasterBand(i+1).WriteArray(img[i])
+        if(band == 1):
+            driver.GetRasterBand(i+1).WriteArray(img)
+
+###############################################################################
+
+igdal = gdal.Open(inp)
+gt = igdal.GetGeoTransform()
+proj = igdal.GetProjection()
+row = igdal.RasterYSize
+col = igdal.RasterXSize
+iarr = igdal.ReadAsArray().astype(np.uint8)
+
+k = 4
+kmeans = np.array(ck.kmeans_calc(iarr, row, col, k), dtype=np.uint8)
+writearr(out, kmeans, 1, col, row, 1, gt, proj, -1)
